1fe13a158fad0ee4a55c5da8cffee7190c5890a0
root <godofluck89@gmail.com>
2026-02-22 12:34:31 +0000

    add tokio proxy

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,19 @@
+use std::thread;
+
+use crate::constructors::{node::Node, proxy::Proxy};
+
 mod constructors;
 
-use constructors::node::Node;
-use constructors::sniffer::Sniffer;
-use std::panic;
-use std::process;
-use std::sync::Arc;
+#[tokio::main]
+async fn main () {
+    let hosts: Vec<String> = vec!["google.com".to_string()];
+    let node = Node::new(hosts.clone());
+    let node_for_proxy = node.clone();
+    let proxy = Proxy::new("0.0.0.0:8080".to_string(), hosts.clone(), node_for_proxy);
 
-fn main () {
-    let mut target_hosts: Vec<String> = Vec::new();
-    target_hosts.push("google.com".to_string());
-
-    let node = Node::new(target_hosts.clone());
-    let node_for_sniffer = Arc::clone(&node);
-
-    panic::set_hook(Box::new(|panic_info| {
-        eprintln!("Критическая ошибка в потоке: {:?}", panic_info);
-        // Грохаем весь процесс с кодом ошибки
-        process::exit(1);
-    }));
-
-    std::thread::spawn(move || {
+    thread::spawn(move || {
         Node::start(&node);
     });
 
-    let sniffer = Sniffer::new(node_for_sniffer, target_hosts.clone());
-    Sniffer::start(sniffer);
+    Proxy::start(proxy).await;
 }
