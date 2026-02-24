6cb4da014df51286bcce333a9dbc51a7983c62ae
root <godofluck89@gmail.com>
2026-02-24 11:08:15 +0000

    add user input

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,31 @@
-use std::{error::Error, sync::Arc, process::exit};
-use guardi_vpn::network::p2p::P2PEngine;
+use std::{sync::Arc};
+use guardi_vpn::{network::p2p::P2PEngine, terminal::user_input::App};
+use color_eyre::Result;
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
+async fn main() -> Result<()> {
     
+    // === P2P Engin ===
     let p2p = Arc::new(P2PEngine::new());
     let p2p_c = p2p.clone();
-    let p2p_handle = tokio::spawn(async move {
-        let _ = p2p_c.listen().await;
-    });
+    // let p2p_handle = tokio::spawn(async move {
+        // let _ = p2p_c.listen().await;
+    // });
 
     let mut tx = p2p.broadcast.subscribe();
 
-    loop {
-        if p2p_handle.is_finished() {
-            println!("P2P Engine is down, exit!");
-            exit(1);
-        }
-        tokio::select! {
-            event = tx.recv() => {
-                println!("P2P Event {:?}", event);
-            }
-        }
-    }
+    // === TERMINAL ===
+    App::default()
+
+    // loop {
+    //     // if p2p_handle.is_finished() {
+    //     //     println!("P2P Engine is down, exit!");
+    //     //     exit(1);
+    //     // }
+    //     // tokio::select! {
+    //     //     event = tx.recv() => {
+    //     //         println!("P2P Event {:?}", event);
+    //     //     }
+    //     // }
+    // }
 }
